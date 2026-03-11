0ae6d368c270bbf05bb98fcd04acc95283054f36
Yitao Zhang <yitao_zhang1999@outlook.com>
2026-03-11 15:30:04 +0800

    Writing the processing.py file in the Python package named data_check

---
+++ data_check/processing.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project     ：STEP1-Data Processing-Built by Yitao_Zhang
+@File        ：processing.py
+@IDE         ：PyCharm
+@Author      ：Yitao Zhang.
+@Email       ：yitao_zhang1999@outlook.com
+@Created Date：2026/3/11 15:22
+"""
+import pandas as pd
+import numpy as np
+
+
